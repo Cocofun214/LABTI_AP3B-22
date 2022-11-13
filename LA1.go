d976f876cf14744483860054f2d4280b4a20e5ee
-----J---- <108605984+Cocofun214@users.noreply.github.com>
2022-11-14 02:02:13 +0700

    Create LA1.go

---
+++ LA1.go
@@ -0,0 +1,12 @@
+package main
+import (
+  "fmt"
+)
+func main() {
+  fmt.Println("Masukkan Tinggi Badan : ") //Meminta Input
+  var tinggi int // Mendeklarasikan variabel tinggi dengan integer
+  fmt.Scanln(&tinggi) // Membaca/Scaning input 
+  if tinggi >= 165 { //Kondisi Jika tinggi Lebih Besar Sama Dengan 165
+    fmt.Println("Anda LOLOS") // Jika Benar 
+  } else {
+    fmt.Println("Kamu Tidak Memenuhi Syarat") //Jika Tidak
